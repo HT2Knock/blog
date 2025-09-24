578e9c2d455021bc18bbdf7fb6d5ef1f8c33b742
T2Knock <huynhtransongngoc@gmail.com>
2025-09-24 11:53:12 +0700

    feat: testing out island with preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [icon(), preact()],
   site: "https://blog-puce-nine-66.vercel.app",
 });
-
