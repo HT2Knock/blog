cf290225dc1784098a764ae7a18a1162975af861
T2Knock <huynhtransongngoc@gmail.com>
2025-10-09 11:52:56 +0700

    chore: add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
-import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), preact()],
+  integrations: [icon(), react()],
   site: "https://blog-puce-nine-66.vercel.app",
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
