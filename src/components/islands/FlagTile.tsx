55bb1332d57d66890859323c50ac58813928ca53
T2Knock <huynhtransongngoc@gmail.com>
2025-10-12 21:55:02 +0700

    --wip-- [skip ci]

--- src/components/islands/FlagTile.tsx
+++ src/components/islands/FlagTile.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useRef } from "react";
+
 export const FlagTile = () => {
+  const flagRef = useRef(null);
+
+  useEffect(() => {}, []);
+
   return (
-    <div>
-      <p>This is the flag tile</p>
-    </div>
+    <svg
+      className="translsate-1/2 h-auto w-full max-w-xs"
+      width="900"
+      height="600"
+      viewBox="0 0 30 20"
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+    >
+      <g ref={flagRef}>
+        <rect width="30" height="20" fill="#C8102E" />
+        <polygon
+          fill="#FFCC00"
+          points="15,4 11.47,14.85 20.71,8.15 9.29,8.15 18.53,14.85"
+        />
+      </g>
+    </svg>
   );
 };
