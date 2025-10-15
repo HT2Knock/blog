10c3051c029aa414557f05db3c69119504e6e534
T2Knock <huynhtransongngoc@gmail.com>
2025-10-15 10:27:07 +0700

    style: simplify the bento grid color

--- src/components/islands/FlagTile.tsx
+++ src/components/islands/FlagTile.tsx
@@ -1,26 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 export const FlagTile = () => {
-  const flagRef = useRef(null);
-
   useEffect(() => {}, []);
 
   return (
-    <svg
-      className="translsate-1/2 h-auto w-full max-w-xs"
-      width="900"
-      height="600"
-      viewBox="0 0 30 20"
-      xmlns="http://www.w3.org/2000/svg"
-      version="1.1"
-    >
-      <g ref={flagRef}>
+    <div className="rounded-2xl border border-primary/20 bg-primary/10 p-4 shadow-lg backdrop-blur-md lg:row-span-2 dark:shadow-black/50">
+      <svg
+        className="rounded"
+        viewBox="0 0 30 20"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+      >
         <rect width="30" height="20" fill="#C8102E" />
         <polygon
           fill="#FFCC00"
           points="15,4 11.47,14.85 20.71,8.15 9.29,8.15 18.53,14.85"
         />
-      </g>
-    </svg>
+      </svg>
+      <h3 className="mt-8 text-2xl">Built with ❤️ + caffeine</h3>
+    </div>
   );
 };
